f723582595230b925a4619fd38d17767db99d1a5
yeffasol <yeffasol@mail.ru>
2016-05-16 20:52:45 +0300

    not ready

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,15 @@
 'use strict';
-(function () {})();
+(function () {
+  jQuery(function ($) {
+    $('.phone').each(function () {
+      $(this).mask("8 999 999 99 99");
+    })
+    $('body').noisy({
+      'intensity': 0,
+      'size': 200,
+      'opacity': 0.08,
+      'fallback': '',
+      'monochrome': false
+    }).css('background-color', '#f0f5e5');
+  });
+})();
